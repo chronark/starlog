64301ee93671bf2966fc2b3378ccf84194e08fa4
Andreas Thomas <dev@chronark.com>
2023-01-21 19:03:15 +0100

    feat: add extrapolate

--- .contentlayer/generated/Project/_index.mjs
+++ .contentlayer/generated/Project/_index.mjs
@@ -2,6 +2,7 @@
 
 import projects__dubMdx from "./projects__dub.mdx.json" assert { type: "json" };
 import projects__envshareMdx from "./projects__envshare.mdx.json" assert { type: "json" };
+import projects__extrapolateMdx from "./projects__extrapolate.mdx.json" assert { type: "json" };
 import projects__precedentMdx from "./projects__precedent.mdx.json" assert { type: "json" };
 import projects__restorephotosMdx from "./projects__restorephotos.mdx.json" assert { type: "json" };
 import projects__starlogMdx from "./projects__starlog.mdx.json" assert { type: "json" };
@@ -10,6 +11,7 @@ import projects__twitterbioMdx from "./projects__twitterbio.mdx.json" assert { t
 export const allProjects = [
   projects__dubMdx,
   projects__envshareMdx,
+  projects__extrapolateMdx,
   projects__precedentMdx,
   projects__restorephotosMdx,
   projects__starlogMdx,
