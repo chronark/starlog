3ea5681872173dc41b085e2a7ec10a879cf71adc
Mark Anthony Llego <111579522+llegomark@users.noreply.github.com>
2023-01-22 23:06:34 +0800

    fix: horizontal overflow

--- app/components/banner.tsx
+++ app/components/banner.tsx
@@ -4,7 +4,7 @@ import React, { useEffect, useState } from "react";
 
 export const Banner: React.FC = () => {
   return (
-    <div className="w-screen bg-black font-display">
+    <div className="w-full bg-black font-display">
       <div className="container flex items-center justify-center px-4 py-2 mx-auto text-sm text-stone-50 ">
         <Link className="hover:underline" target="_blank" href="https://github.com/chronark/starlog">
           STARLOG is open source on <span className="font-medium">GitHub</span>
